543209724443090d52139509dd6cedf83ea85770
Tony DiNitto <think41c@gmail.com>
2015-04-10 22:41:01 -0600

    Basic framework estalbished.

--- atm.rb
+++ atm.rb
@@ -1,11 +1,11 @@
-class ATM
-  def initialize
+class Atm
+  def initialize(withdrawal = 0)
     
   end
 
   def balance
     2000 
   end
-
-
 end
+
+# bill = Atm.new
--- atm_test.rb
+++ atm_test.rb
@@ -1,11 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-
 require_relative 'atm'
 
-class ATMTest < Minitest::Test
+
+class AtmTest < Minitest::Test
+
+  def test_it_exists
+    assert true
+  end
+
   def test_it_has_a_balance
     billy_atm = Atm.new
     assert_equal 2000, billy_atm.balance
   end
+
 end 
