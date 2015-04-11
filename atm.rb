ebe5efd595f94fac5d920f8bf37822b13272b178
Tony DiNitto <think41c@gmail.com>
2015-04-10 22:46:44 -0600

    Basic input being handled.

--- atm.rb
+++ atm.rb
@@ -1,11 +1,15 @@
 class Atm
-  def initialize(withdrawal = 0)
-    
+  def initialize(withdrawal_and_balance = "")
+    input = withdrawal_and_balance.split
+    withdrawal = input[0]
+    balance    = input[1]
+    puts "withdrawal #{withdrawal} and balance: #{balance}"
   end
 
   def balance
     2000 
   end
+
 end
 
-# bill = Atm.new
+bill = Atm.new("10 130.00")
