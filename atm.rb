0b59a8ff6a02803d21652119654c819cb84a6afa
Tony DiNitto <think41c@gmail.com>
2015-04-10 22:50:57 -0600

    More logic implemented to parse input.

--- atm.rb
+++ atm.rb
@@ -1,9 +1,22 @@
 class Atm
   def initialize(withdrawal_and_balance = "")
     input = withdrawal_and_balance.split
-    withdrawal = input[0]
-    balance    = input[1]
-    puts "withdrawal #{withdrawal} and balance: #{balance}"
+    @withdrawal = input[0]
+    @balance    = input[1]
+    puts "withdrawal: #{@withdrawal} and balance: #{@balance}"
+    puts @withdrawal.to_i % 5 
+    valid_input
+
+  end
+
+  def valid_input
+    if @withdrawal.to_i % 5 == 0 
+      puts "Valid multiple for withdrawal"
+    else
+      puts "Not a valid withdrawal amount"
+      exit
+    end
+
   end
 
   def balance
